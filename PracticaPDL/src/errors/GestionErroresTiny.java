4c787eb013d74e003cfdafe17bdd6209d909ee7a
Jorge Villarrubia <georgeatletico@gmail.com>
2020-04-21 17:00:41 +0200

    Update GestionErroresTiny.java

--- PracticaPDL/src/errors/GestionErroresTiny.java
+++ PracticaPDL/src/errors/GestionErroresTiny.java
@@ -3,12 +3,13 @@ package errors;
 import alex.UnidadLexica;
 
 public class GestionErroresTiny {
-   public void errorLexico(int fila, String lexema) {
-     System.out.println("ERROR fila "+fila+": Caracter inexperado: "+lexema); 
+   public static void errorLexico(int fila, int columna, String lexema) {
+     System.err.println("***ERROR léxico. Fila: "+ fila + ", Columna: " + columna + ": Elemento inesperado " + lexema); 
      System.exit(1);
    }  
    public void errorSintactico(UnidadLexica unidadLexica) {
-     System.out.print("ERROR fila "+unidadLexica.fila()+": Elemento inexperado "+unidadLexica.value);
+     System.err.print("***ERROR sintáctico. Fila: "+ unidadLexica.fila() + ", Columna: " + unidadLexica.columna() + ": Elemento inesperado "+ unidadLexica.value);
      System.exit(1);
    }
 }
+
